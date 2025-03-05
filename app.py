3c82e7be28675bbadcd73b1c92cc42902bfd1d4c
Belinda Velazquez <belinda.datanalyst@outlook.com>
2025-03-05 16:08:19 -0600

    agregamos titulo y histograma

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header('Analisis de veiculos')
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -14,3 +15,9 @@ if hist_button: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
